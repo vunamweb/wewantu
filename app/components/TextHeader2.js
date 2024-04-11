4236f30dae5598db53d94aa44711b4e092904479
vunamweb <vukynamkhtn@gmail.com>
2024-04-11 23:30:42 +0700

    change color and background when hover

--- app/components/TextHeader2.js
+++ app/components/TextHeader2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Image, PixelRatio, Dimensions } from "react-native";
 
 import Text from "./Paragraph";
@@ -25,24 +25,72 @@ export default function TextHeader2({
   callBack2,
   callBack3,
 }) {
+  const [color1, setColor1] = useState('#898166');
+  const [bg1, setBg1] = useState('#fff');
+  const [color2, setColor2] = useState('#898166');
+  const [bg2, setBg2] = useState('#fff');
+  const [color3, setColor3] = useState('#898166');
+  const [bg3, setBg3] = useState('#fff');
+
+  const goToWork = () => {
+    setColor1('#fff');
+    setBg1('#898166');
+
+    setColor2('#898166');
+    setBg2('#fff');
+
+    setColor3('#898166');
+    setBg3('#fff');
+
+    callBack1();
+  };
+
+  const goToLife = () => {
+    setColor2('#fff');
+    setBg2('#898166');
+
+    setColor1('#898166');
+    setBg1('#fff');
+
+    setColor3('#898166');
+    setBg3('#fff');
+
+    callBack2();
+  };
+
+  const goToBalance = () => {
+    setColor3('#fff');
+    setBg3('#898166');
+
+    setColor2('#898166');
+    setBg2('#fff');
+
+    setColor1('#898166');
+    setBg1('#fff');
+
+    callBack3();
+  };
+
+  
+  
   var view1 = (
-    <Href onPress={() => callBack1()} style={[style.rotatedText]}>
-      <Text style={[styles.fontBoldSpecialOfSpecial, style.text1]}>work</Text>
+    <Href onPress={() => goToWork()} style={[style.rotatedText, { backgroundColor: bg1 }]}>
+      <Text style={[styles.fontBoldSpecialOfSpecial, style.text1, { color: color1 }]}>work</Text>
     </Href>
   );
 
   var view2 = (
-    <Href onPress={() => callBack2()} style={[style.rotatedText, { backgroundColor: "#fff" }]}>
-      <Text style={[styles.fontBoldSpecialOfSpecial, style.text2]}>life</Text>
+    <Href onPress={() => goToLife()} style={[style.rotatedText, { backgroundColor: bg2 }]}>
+      <Text style={[styles.fontBoldSpecialOfSpecial, style.text2, { color: color2 }]}>life</Text>
     </Href>
   );
 
   var view3 = (
     <Href
-      onPress={() => callBack3()}
-      style={[style.rotatedText, { backgroundColor: "#fff" }]}
+      onPress={() => goToBalance()}
+      style={[style.rotatedText, { backgroundColor: bg3 }]}
     >
-      <Text style={[styles.fontBoldSpecialOfSpecial, style.text3]}>
+      <Text style={[styles.fontBoldSpecialOfSpecial, style.text3, { color: color3 }]}>
         balance
       </Text>
     </Href>
@@ -50,7 +98,7 @@ export default function TextHeader2({
   return (
     <View style={[style.view]}>
       <View style={{ marginTop: -30, paddingLeft: 50 }}>{view1}</View>
-      <View style={{ marginLeft: -70 }}>{view2}</View>
+      <View style={{ marginLeft: -73, zIndex: 999999 }}>{view2}</View>
       <View style={{ marginLeft: -110, marginTop: -68 }}>{view3}</View>
 
       {/*<Href style={[styles.fontBoldSpecialOfSpecial, style.rotatedText, style.text3]}>
@@ -107,7 +155,7 @@ const style = StyleSheet.create({
   },
 
   text2: {
-    color: "#898166",
+    //color: "#898166",
     textTransform: "uppercase",
     marginTop: -5,
     //backgroundColor: '#898166',
@@ -116,7 +164,7 @@ const style = StyleSheet.create({
   text3: {
     textTransform: "uppercase",
     marginTop: -5,
-    color: "#898166",
+    //color: "#898166",
   },
 
   rotatedText: {
