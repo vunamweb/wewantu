5d0bdf6f551c5c01c1047075600681f1e26dca9f
vunamweb <vukynamkhtn@gmail.com>
2024-04-12 16:35:05 +0700

    work life balance
    caculate average value of bar

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -1,5 +1,12 @@
 import React, { Component, createRef } from "react";
-import { Text, View, SafeAreaView, Dimensions, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
 
 import Carousel, { Pagination } from "react-native-snap-carousel";
 
@@ -7,8 +14,28 @@ import Href from "../components/Href";
 import Image from "../components/Image";
 import functions from "../function/function";
 
+import styles from "../../app/style/style";
+
 const imgBack = require("../images/arrow_back.png");
 const imgNext = require("../images/arrow_next.png");
+
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 export default class MyCarousel extends React.Component {
   constructor(props) {
@@ -17,29 +44,29 @@ export default class MyCarousel extends React.Component {
     this.carousel = createRef();
 
     this.state = {
-      activeIndex: (this.props.activeIndex) ? this.props.activeIndex : 0,
+      activeIndex: this.props.activeIndex ? this.props.activeIndex : 0,
       height: 0,
       animating: false,
       loading: true,
+      ActivityIndicator: false,
     };
   }
 
-  
   componentDidMount() {}
 
   back = () => {
-     if(this.props.component.index == -1)
-     this.carousel.current.snapToPrev();
-     else 
-     functions.gotoScreen(this.props.component.props.navigation, 'Wlb');
-  }
+    if (this.props.component.index == -1) {
+      this.setState({ ActivityIndicator: true });
+      this.carousel.current.snapToPrev();
+    } else functions.gotoScreen(this.props.component.props.navigation, "Wlb");
+  };
 
   next = () => {
-    if(this.props.component.index == -1)
-    this.carousel.current.snapToNext();
-    else 
-    functions.gotoScreen(this.props.component.props.navigation, 'Wlb');
- }
+    if (this.props.component.index == -1) {
+      this.setState({ ActivityIndicator: true });
+      this.carousel.current.snapToNext();
+    } else functions.gotoScreen(this.props.component.props.navigation, "Wlb");
+  };
 
   pagination = () => {
     return (
@@ -55,31 +82,54 @@ export default class MyCarousel extends React.Component {
     );
   };
 
+  pagination_1 = () => {
+    return (
+      <View style={style.pagination}>
+        <Text style={[styles.fontBoldSmall, style.textChart]}>
+          {this.props.activeIndex != this.state.activeIndex
+            ? monthNames[this.state.activeIndex]
+            : monthNames[this.props.activeIndex]}
+        </Text>
+        <Text style={[styles.fontBoldSmall, style.textChart]}>
+          {currentYear}
+        </Text>
+      </View>
+    );
+  };
+
   render() {
-    var pagination = this.props.pagination ? this.pagination() : this.props.pagination_1();
+    var pagination = this.props.pagination
+      ? this.pagination()
+      : this.pagination_1();
+
+    var display = (this.state.ActivityIndicator) ? 'none' : 'flex';   
 
     return (
       <View style={this.props.styleParent}>
+        <ActivityIndicator
+          size="small"
+          animating={this.state.ActivityIndicator}
+        />
         <Carousel
           ref={this.carousel}
           sliderWidth={Dimensions.get("window").width}
           firstItem={this.state.activeIndex}
           {...this.props}
-          onSnapToItem={(index) => this.setState({ activeIndex: index, height: 0 })}
+          onSnapToItem={(index) =>
+            this.setState({
+              activeIndex: index,
+              height: 0,
+              ActivityIndicator: false,
+            })
+          }
           //inactiveSlideOpacity={1}
         />
         <View>
           {pagination}
-          <Href
-            style={style.back}
-            onPress={() => this.back()}
-          >
+          <Href style={[style.back, { display: display }]} onPress={() => this.back()}>
             <Image source={imgBack} />
           </Href>
-          <Href
-            style={style.next}
-            onPress={() => this.next()}
-          >
+          <Href style={[style.next, { display: display }]} onPress={() => this.next()}>
             <Image source={imgNext} />
           </Href>
         </View>
@@ -110,12 +160,35 @@ const style = StyleSheet.create({
   back: {
     position: "absolute",
     left: 20,
-    top: 10,
+    top: 0,
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingBottom: 20,
+    paddingTop: 10,
+    //backgroundColor: 'red'
   },
 
   next: {
     position: "absolute",
     right: 0,
-    top: 10,
+    top: 0,
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingBottom: 20,
+    paddingTop: 10,
+    //backgroundColor: 'red'
+  },
+
+  pagination: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
+    marginTop: 10,
+    //flex: 2
+  },
+
+  textChart: {
+    color: "#fff",
+    marginLeft: 5,
   },
 });
--- app/screen/WorkChart.js
+++ app/screen/WorkChart.js
@@ -75,6 +75,36 @@ const monthNames = [
   "December",
 ];
 
+const svgCode1 = `
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="69.386" viewBox="0 0 50 69.386">
+  <g id="Group_523" data-name="Group 523" transform="translate(0 0)">
+    <g id="Group_525" data-name="Group 525" transform="translate(0 0)">
+      <path id="Path_247" data-name="Path 247" d="M48.588,61.99a5.979,5.979,0,0,1-5.966,5.984H6.88a6.024,6.024,0,0,1-6-5.984L.708,6.692A5.98,5.98,0,0,1,6.674.708H42.416a6.024,6.024,0,0,1,6,5.984Z" transform="translate(0.351 0.351)" fill="#e4e4e4"/>
+      <path id="Path_248" data-name="Path 248" d="M42.974,69.386H7.231a7.076,7.076,0,0,1-7.062-7.04L0,7.046A7.035,7.035,0,0,1,7.025,0H42.767a7.075,7.075,0,0,1,7.062,7.04L50,62.338a7.041,7.041,0,0,1-7.026,7.047M7.025,2.12A4.918,4.918,0,0,0,2.12,7.04l.169,55.3a4.952,4.952,0,0,0,4.943,4.928H42.974a4.921,4.921,0,0,0,4.907-4.92h0l-.171-55.3A4.952,4.952,0,0,0,42.767,2.12Z" transform="translate(0 0)" fill="#898166"/>
+      <path id="Path_249" data-name="Path 249" d="M20.44,24.823a1.24,1.24,0,0,1-.877-2.117l7.442-7.444a1.24,1.24,0,1,1,1.755,1.753L21.317,24.46a1.233,1.233,0,0,1-.877.364" transform="translate(9.522 7.389)" fill="#898166"/>
+      <path id="Path_250" data-name="Path 250" d="M27.884,24.823a1.233,1.233,0,0,1-.877-.364l-7.444-7.444a1.24,1.24,0,1,1,1.755-1.753l7.442,7.444a1.24,1.24,0,0,1-.877,2.117" transform="translate(9.522 7.389)" fill="#898166"/>
+      <path id="Path_251" data-name="Path 251" d="M16.274,24.823a1.233,1.233,0,0,1-.877-.364L7.954,17.016a1.24,1.24,0,1,1,1.755-1.753l7.442,7.444a1.24,1.24,0,0,1-.877,2.117" transform="translate(3.764 7.389)" fill="#898166"/>
+      <path id="Path_252" data-name="Path 252" d="M8.83,24.823a1.24,1.24,0,0,1-.877-2.117L15.4,15.263a1.24,1.24,0,1,1,1.755,1.753L9.707,24.46a1.233,1.233,0,0,1-.877.364" transform="translate(3.764 7.389)" fill="#898166"/>
+      <path id="Path_253" data-name="Path 253" d="M26.2,33.952a1.233,1.233,0,0,1-.877-.364L21.234,29.5,17.15,33.588a1.238,1.238,0,0,1-1.755,0L11.312,29.5l-1.605,1.6a1.24,1.24,0,0,1-1.753-1.753l2.48-2.48a1.238,1.238,0,0,1,1.755,0l4.085,4.084,4.084-4.084a1.238,1.238,0,0,1,1.755,0L26.2,30.957l4.085-4.084a1.236,1.236,0,0,1,1.753,0l2.482,2.48a1.24,1.24,0,0,1-1.755,1.753l-1.6-1.6-4.085,4.085a1.233,1.233,0,0,1-.877.364" transform="translate(3.764 13.147)" fill="#898166"/>
+    </g>
+  </g>
+</svg>
+`;
+
+const svgCode2 = `
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="69.386" viewBox="0 0 50 69.386">
+  <g id="Group_524" data-name="Group 524" transform="translate(0 0)">
+    <g id="Group_528" data-name="Group 528" transform="translate(0 0)">
+      <path id="Path_254" data-name="Path 254" d="M48.588,61.99a5.979,5.979,0,0,1-5.966,5.984H6.88a6.024,6.024,0,0,1-6-5.984L.708,6.692A5.98,5.98,0,0,1,6.674.708H42.416a6.024,6.024,0,0,1,6,5.984Z" transform="translate(0.351 0.351)" fill="#e4e4e4"/>
+      <path id="Path_255" data-name="Path 255" d="M42.974,69.386H7.231a7.076,7.076,0,0,1-7.062-7.04L0,7.046A7.035,7.035,0,0,1,7.025,0H42.767a7.075,7.075,0,0,1,7.062,7.04L50,62.338a7.041,7.041,0,0,1-7.026,7.047M7.025,2.12A4.918,4.918,0,0,0,2.12,7.04l.169,55.3a4.952,4.952,0,0,0,4.943,4.928H42.974a4.921,4.921,0,0,0,4.907-4.92h0l-.171-55.3A4.952,4.952,0,0,0,42.767,2.12Z" transform="translate(0 0)" fill="#898166"/>
+      <path id="Path_256" data-name="Path 256" d="M15.863,19.035a3.722,3.722,0,1,1-3.722-3.722,3.722,3.722,0,0,1,3.722,3.722" transform="translate(4.175 7.594)" fill="#898166"/>
+      <path id="Path_257" data-name="Path 257" d="M27.473,19.035a3.722,3.722,0,1,1-3.722-3.722,3.722,3.722,0,0,1,3.722,3.722" transform="translate(9.933 7.594)" fill="#898166"/>
+      <path id="Path_258" data-name="Path 258" d="M27.857,33.538a1.245,1.245,0,0,1-.343-.048L10.147,28.528a1.241,1.241,0,0,1,.682-2.386L28.2,31.1a1.241,1.241,0,0,1-.34,2.434" transform="translate(4.586 12.941)" fill="#898166"/>
+    </g>
+  </g>
+</svg>
+`;
+
 const svgCode3 = `
 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="69.386" viewBox="0 0 50 69.386">
   <g id="Group_525" data-name="Group 525" transform="translate(0 0)">
@@ -89,12 +119,40 @@ const svgCode3 = `
 </svg>
 `;
 
+const svgCode4 = `
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="69.386" viewBox="0 0 50 69.386">
+  <g id="Group_526" data-name="Group 526" transform="translate(0 0)">
+    <g id="Group_534" data-name="Group 534" transform="translate(0 0)">
+      <path id="Path_264" data-name="Path 264" d="M48.588,61.99a5.979,5.979,0,0,1-5.966,5.984H6.88a6.024,6.024,0,0,1-6-5.984L.708,6.692A5.98,5.98,0,0,1,6.674.708H42.416a6.024,6.024,0,0,1,6,5.984Z" transform="translate(0.351 0.351)" fill="#e4e4e4"/>
+      <path id="Path_265" data-name="Path 265" d="M42.974,69.386H7.231a7.076,7.076,0,0,1-7.062-7.04L0,7.046A7.035,7.035,0,0,1,7.025,0H42.767a7.075,7.075,0,0,1,7.062,7.04L50,62.338a7.041,7.041,0,0,1-7.026,7.047M7.025,2.12A4.918,4.918,0,0,0,2.12,7.04l.169,55.3a4.952,4.952,0,0,0,4.943,4.928H42.974a4.921,4.921,0,0,0,4.907-4.92h0l-.171-55.3A4.952,4.952,0,0,0,42.767,2.12Z" transform="translate(0 0)" fill="#898166"/>
+      <path id="Path_266" data-name="Path 266" d="M15.863,18.2a3.722,3.722,0,1,1-3.722-3.722A3.722,3.722,0,0,1,15.863,18.2" transform="translate(4.175 7.183)" fill="#898166"/>
+      <path id="Path_267" data-name="Path 267" d="M27.473,18.2a3.722,3.722,0,1,1-3.722-3.722A3.722,3.722,0,0,1,27.473,18.2" transform="translate(9.933 7.183)" fill="#898166"/>
+      <path id="Path_268" data-name="Path 268" d="M20.306,35.19a11.155,11.155,0,0,1-10.8-8.374,1.24,1.24,0,1,1,2.4-.619,8.672,8.672,0,0,0,16.8,0,1.241,1.241,0,0,1,2.4.619,11.156,11.156,0,0,1-10.8,8.374" transform="translate(4.694 12.529)" fill="#898166"/>
+    </g>
+  </g>
+</svg>
+`;
+
+const svgCode5 = `
+<svg xmlns="http://www.w3.org/2000/svg" width="50" height="69.386" viewBox="0 0 50 69.386">
+  <g id="Group_527" data-name="Group 527" transform="translate(0 0)">
+    <g id="Group_537" data-name="Group 537" transform="translate(0 0)">
+      <path id="Path_269" data-name="Path 269" d="M48.588,61.99a5.979,5.979,0,0,1-5.966,5.984H6.88a6.024,6.024,0,0,1-6-5.984L.708,6.692A5.98,5.98,0,0,1,6.674.708H42.416a6.024,6.024,0,0,1,6,5.984Z" transform="translate(0.351 0.351)" fill="#e4e4e4"/>
+      <path id="Path_270" data-name="Path 270" d="M42.974,69.386H7.231a7.076,7.076,0,0,1-7.062-7.04L0,7.046A7.035,7.035,0,0,1,7.025,0H42.767a7.075,7.075,0,0,1,7.062,7.04L50,62.338a7.041,7.041,0,0,1-7.026,7.047M7.025,2.12A4.918,4.918,0,0,0,2.12,7.04l.169,55.3a4.952,4.952,0,0,0,4.943,4.928H42.974a4.921,4.921,0,0,0,4.907-4.92h0l-.171-55.3A4.952,4.952,0,0,0,42.767,2.12Z" transform="translate(0 0)" fill="#898166"/>
+      <path id="Path_271" data-name="Path 271" d="M17.1,20.788a1.24,1.24,0,0,1-1.24-1.24,3.1,3.1,0,1,0-6.2,0,1.24,1.24,0,0,1-2.48,0,5.582,5.582,0,1,1,11.164,0,1.24,1.24,0,0,1-1.24,1.24" transform="translate(3.558 6.926)" fill="#898166"/>
+      <path id="Path_272" data-name="Path 272" d="M28.709,20.788a1.24,1.24,0,0,1-1.24-1.24,3.1,3.1,0,1,0-6.2,0,1.24,1.24,0,0,1-2.48,0,5.582,5.582,0,1,1,11.164,0,1.24,1.24,0,0,1-1.24,1.24" transform="translate(9.316 6.926)" fill="#898166"/>
+      <path id="Path_273" data-name="Path 273" d="M20.306,35.708a11.155,11.155,0,0,1-10.8-8.374,1.239,1.239,0,0,1,1.2-1.55H29.9a1.239,1.239,0,0,1,1.2,1.55,11.153,11.153,0,0,1-10.8,8.374m-7.839-7.444a8.67,8.67,0,0,0,15.676,0Z" transform="translate(4.694 12.787)" fill="#898166"/>
+    </g>
+  </g>
+</svg>
+`;
+
 class WorkChart extends Component {
   constructor(props) {
     super(props);
 
     this._renderItem.bind(this);
-    this.pagination_1.bind(this);
+    //this.pagination_1.bind(this);
 
     this.carousel = createRef();
     this.video = [];
@@ -195,21 +253,6 @@ class WorkChart extends Component {
     );
   };
 
-  pagination_1 = () => {
-    return (
-      <View style={style.pagination}>
-        <Text style={[styles.fontBoldSmall, style.textChart]}>
-          {this.carousel.current != undefined
-            ? monthNames[this.carousel.current.state.activeIndex]
-            : null}
-        </Text>
-        <Text style={[styles.fontBoldSmall, style.textChart]}>
-          {currentYear}
-        </Text>
-      </View>
-    );
-  };
-
   initChart = async () => {
     var chart_work = await functions.getDataAsyncStorage(strStorage);
 
@@ -231,7 +274,110 @@ class WorkChart extends Component {
     return data;
   };
 
+  averageMonth = () => {
+    var data = this.state.chart;
+
+    var count = 0;
+    var sum = 0;
+
+    try {
+      for (let i = 0; i <= 11; i++) {
+        for (let j = 1; j <= 30; j++)
+          if (data[i][j] != 0 && data[i][j] != undefined && (i + 1 == currentMonth)) {
+            sum = sum + data[i][j];
+            count++;
+          }
+      }
+    } catch (error) {
+      console.log(error);
+      return 1;
+    }
+
+    return Math.round(sum / count);
+  };
+
+  averageYear = () => {
+    var data = this.state.chart;
+
+    var count = 0;
+    var sum = 0;
+
+    try {
+      for (let i = 0; i <= 11; i++) {
+        for (let j = 1; j <= 30; j++)
+          if (data[i][j] != 0 && data[i][j] != undefined) {
+            sum = sum + data[i][j];
+            count++;
+          }
+      }
+    } catch (error) {
+      console.log(error);
+      return 1;
+    }
+
+    return Math.round(sum / count);
+  };
+
+  getIconHappyMonth = () => {
+    let value = this.averageMonth();
+
+    switch (value) {
+      case 2:
+        return svgCode2;
+        break;
+
+      case 3:
+        return svgCode3;
+        break;
+
+      case 4:
+        return svgCode4;
+        break;
+
+      case 5:
+        return svgCode5;
+        break;
+
+      default:
+        return svgCode1;
+    }
+  };
+
+  getIconHappyYear = () => {
+    let value = this.averageYear();
+
+    switch (value) {
+      case 2:
+        return svgCode2;
+        break;
+
+      case 3:
+        return svgCode3;
+        break;
+
+      case 4:
+        return svgCode4;
+        break;
+
+      case 5:
+        return svgCode5;
+        break;
+
+      default:
+        return svgCode1;
+    }
+  };
+
   render() {
+    var commonData = global.commonData.languages;
+
+    try {
+      var text1 = commonData.happyness_in;
+      var text2 = commonData.happyness_in_year;
+} catch (error) {
+      console.log(error);
+    }
+
     var chart = this.state.chart;
     var title;
 
@@ -279,34 +425,35 @@ class WorkChart extends Component {
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
           <Background>
             <TextHeader text1={title} style={style.textHeader} />
-            <Carousel
-              data={chart}
-              ref={this.carousel}
-              activeIndex={currentMonth - 1}
-              styleParent={style.styleParent}
-              stylePagination={style.stylePagination}
-              pagination_1={() => this.pagination_1()}
-              component={this}
-              renderItem={this._renderItem}
-              itemWidth={Dimensions.get("window").width}
-              loop={false}
-            />
+            {chart.length > 0 ? (
+              <Carousel
+                data={chart}
+                ref={this.carousel}
+                activeIndex={currentMonth - 1}
+                styleParent={style.styleParent}
+                stylePagination={style.stylePagination}
+                pagination_1={() => null}
+                component={this}
+                renderItem={this._renderItem}
+                itemWidth={Dimensions.get("window").width}
+                loop={false}
+              />
+            ) : null}
+
             <View style={style.checkbox}>
               <View style={style.statusFeeling}>
                 <Text style={[styles.textAlignCenter, styles.fontBoldSmall]}>
-                  THROUGHOUT{"\n"}
-                  HAPPYNESS IN{"\n"}
+                  {text1}{"\n"}
                   {monthNames[currentMonth - 1]}
                 </Text>
-                <SvgXml xml={svgCode3} width="50" height="70" />
+                <SvgXml style={style.happy} xml={this.getIconHappyMonth()} width="50" height="70" />
               </View>
               <View style={style.statusFeeling}>
                 <Text style={[styles.textAlignCenter, styles.fontBoldSmall]}>
-                  INTERPRETATIONS{"\n"}
-                  HAPPYNESS IN{"\n"}
-                  2023
+                  {text2}{"\n"}
+                  {currentYear}
                 </Text>
-                <SvgXml xml={svgCode3} width="50" height="70" />
+                <SvgXml style={style.happy} xml={this.getIconHappyYear()} width="50" height="70" />
               </View>
             </View>
             <BackNext
@@ -367,19 +514,6 @@ const style = StyleSheet.create({
     marginTop: 30,
   },
 
-  pagination: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "center",
-    marginTop: 10,
-    //flex: 2
-  },
-
-  textChart: {
-    color: "#fff",
-    marginLeft: 5,
-  },
-
   statusFeeling: {
     alignItems: "center",
   },
@@ -387,6 +521,10 @@ const style = StyleSheet.create({
   iconFeeling: {
     marginTop: 5,
   },
+
+  happy: {
+    marginTop: 5
+  }
 });
 
 export default WorkChart;
